9957ff52e8b5633b45ee76ae0b6a48f8ea7e8905
DimaPindrik <dyebolical@gmail.com>
2019-08-05 22:17:56 +0300

    Created Difficulty ENUM entity

---
+++ src/main/java/com/devdim/recipeapp/spring5recipeapp/domain/Difficulty.java
@@ -0,0 +1,9 @@
+package com.devdim.recipeapp.spring5recipeapp.domain;
+
+/**
+ * created by Dima on 8/5/2019.
+ */
+public enum Difficulty {
+
+    EASY, MODERATE, HARD
+}
--- src/main/java/com/devdim/recipeapp/spring5recipeapp/domain/Recipe.java
+++ src/main/java/com/devdim/recipeapp/spring5recipeapp/domain/Recipe.java
@@ -21,11 +21,14 @@ public class Recipe {
     private String url;
     private String directions;
 
+    @Lob
+    private Byte[] image;
+
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
     private Set<Ingredient> ingredients;
 
-    @Lob
-    private Byte[] image;
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
 
     @OneToOne(cascade = CascadeType.ALL)
     private Note note;
@@ -108,5 +111,21 @@ public class Recipe {
 
     public void setNote(Note note) {
         this.note = note;
+    }
+
+    public Set<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public void setIngredients(Set<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
